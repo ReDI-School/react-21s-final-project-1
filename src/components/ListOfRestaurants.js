30b593df2653985d1db5916c4f7e5ef77e069a36
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-06-14 08:06:13 +0200

    Filters and Re-Rounting

--- src/components/ListOfRestaurants.js
+++ src/components/ListOfRestaurants.js
@@ -2,65 +2,84 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
+import HomePageGif from '../assets/homepage-gif.gif';
 
 function ListOfRestaurants() {
-  const [data, setData] = useState(null);
+  const [restaurants, setRestaurants] = useState(null);
+  const [filteredRestaurants, setFilteredRestaurants] = useState(null);
+
   useEffect(() => {
     async function loadData() {
       const response = await fetch(
         'https://redi-final-restaurants.herokuapp.com/restaurants/'
       );
-      const data = await response.json();
-      setData(data);
+      const responseJson = await response.json();
+      setRestaurants(responseJson.results);
+      setFilteredRestaurants(responseJson.results);
     }
     loadData();
   }, []);
-  console.log(data);
 
-  // taking each Restaurant as link and show it in the list
-  const Home = () => (
+  // all the Restaurants
+  const handleFilterAll = () => {
+    setFilteredRestaurants(restaurants);
+    console.log(restaurants);
+  };
+  // Pickup Restaurants
+  const handleFilterPickup = () => {
+    const pickupRestaurants = restaurants.filter(
+      (restaurant) => restaurant.pickup
+    );
+
+    setFilteredRestaurants(pickupRestaurants);
+    console.log(pickupRestaurants);
+  };
+  // Open-now Restaurant
+  const handleFilterOpen = () => {
+    const openRestaurants = restaurants.filter(
+      (restaurant) => restaurant.opening_hours.open_now === false
+    );
+
+    setFilteredRestaurants(openRestaurants);
+    console.log(openRestaurants);
+  };
+
+  // Delivery Restaurants
+  const handleFilterDelivery = () => {
+    const deliveryRestaurants = restaurants.filter(
+      (restaurant) => restaurant.delivery
+    );
+
+    setFilteredRestaurants(deliveryRestaurants);
+    console.log(deliveryRestaurants);
+  };
+
+  return (
     <div>
-      {data &&
-        data.results.map((restaurant) => (
-          <div className="restaurantCard" key={restaurant.id}>
-          
-            <img src={restaurant.photos[0].links[0]} />
-          
-            <h1 className="restaurantTitle">
-             <Link
-                to={{
-                  pathname: `/ListOfRestaurants/${restaurant.id}`,
-                  data: {
-                    name: restaurant.name,
-                    cuisine: restaurant.cuisine,
-                    formatted_address: restaurant.formatted_address,
-                    social: restaurant.social,
-                    photos: restaurant.photos,
-                    price_level: restaurant.price_level,
-                    rating: restaurant.rating,
-                    opening_hours: {
-                      opening_hours: restaurant.opening_hours,
-                      hours: {
-                        open: restaurant.opening_hours.open,
-                        close: restaurant.opening_hours.close,
-                      },
-                    },
-                    pickup: restaurant.pickup,
-                    delivery: restaurant.delivery,
-                  },
-                }}>
+      <div>
+        <img src={HomePageGif} alt='' />
+        <button onClick={handleFilterAll}>All </button>
+        <button onClick={handleFilterOpen}>Open </button>
+        <button onClick={handleFilterPickup}>Pickup </button>
+        <button onClick={handleFilterDelivery}>Delivery </button>
+        <SearchBar />
+      </div>
+      {filteredRestaurants &&
+        filteredRestaurants.map((restaurant) => (
+          <div key={restaurant.id}>
+            <img src={restaurant.photos[0].links[0]} alt='' />
+
+            <h1 className='restaurantTitle'>
+              <Link to={`/ListOfRestaurants/${restaurant.id}`}>
                 {restaurant.name}
               </Link>
             </h1>
-            <h3 className="restaurantCousine">{restaurant.cuisine}</h3>
+            <h3>{restaurant.cuisine}</h3>
           </div>
         ))}
     </div>
   );
-  return (
-    <div>
-      <Home  />
-    </div>
-  );
 }
+
 export default ListOfRestaurants;
--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -1,98 +1,73 @@
-import React, { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 // I have to target the id of each restaurant
 
 function RestaurantDetails() {
-  const { data } = useLocation();
-  let history = useHistory();
-  if (!data) {
-    history.push('/ListOfRestaurants');
-  }
+  const { id } = useParams();
+
+  const [restaurant, setRestaurant] = useState(null);
+
   useEffect(() => {
-    async function loadRest() {}
-    loadRest();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    async function loadData() {
+      const response = await fetch(
+        'https://redi-final-restaurants.herokuapp.com/restaurants/'
+      );
+      const responseJson = await response.json();
+      setRestaurant(responseJson.results.find((rest) => rest.id === id));
+    }
+    loadData();
+  }, [id]); // eslint-disable-next-line
 
   /// here you can show the restaurant data, you can apply your styles here.
-  return (
+  return restaurant ? (
     <div>
-      {data && data.name && <h3>Name: {data.name}</h3>}
-      {data && data.cuisine && <h4>Cuisine: {data.cuisine} </h4>}
-      {data && data.formatted_address && (
-        <h5>Address: {data.formatted_address}</h5>
+      {restaurant && restaurant.name && <h3>Name: {restaurant.name}</h3>}
+      {restaurant && restaurant.cuisine && (
+        <h4>Cuisine: {restaurant.cuisine} </h4>
       )}
-      {data && data.social && (
+      {restaurant && restaurant.formatted_address && (
+        <h5>Address: {restaurant.formatted_address}</h5>
+      )}
+      {restaurant && restaurant.social && (
         <h5>
-          Email: {data.social.email} Phone: {data.social.phone}
+          Email: {restaurant.social.email} Phone: {restaurant.social.phone}
         </h5>
       )}
-      {data.photos &&
-        data.photos[0] &&
-        data.photos[0].links &&
-        data.photos[0].links[0] && (
-          <img src={data.photos[0].links[0]} alt='icon' />
+      {restaurant.photos &&
+        restaurant.photos[0] &&
+        restaurant.photos[0].links &&
+        restaurant.photos[0].links[0] && (
+          <img src={restaurant.photos[0].links[0]} alt='icon' />
         )}
-      {data && data.price_level && <h6>Price level {data.price_level}</h6>}
-      {data && data.rating && <h6>Rating: {data.rating}</h6>}
-
-      {data && data.opening_hours && data.opening_hours.open_now && (
-        <div>{data.opening_hours.open_now ? 'open' : 'closed'}</div>
+      {restaurant && restaurant.price_level && (
+        <h6>Price level {restaurant.price_level}</h6>
       )}
-      {data &&
-        data.opening_hours &&
-        data.opening_hours.hours &&
-        data.opening_hours.hours.open &&
-        data.opening_hours.hours.close && (
+      {restaurant && restaurant.rating && <h6>Rating: {restaurant.rating}</h6>}
+
+      {restaurant &&
+        restaurant.opening_hours &&
+        restaurant.opening_hours.open_now && (
+          <div>{restaurant.opening_hours.open_now ? 'open' : 'closed'}</div>
+        )}
+      {restaurant &&
+        restaurant.opening_hours &&
+        restaurant.opening_hours.hours &&
+        restaurant.opening_hours.hours.open &&
+        restaurant.opening_hours.hours.close && (
           <div>
-            <h4>{data.opening_hours.hours.open}</h4>
-            <h4>{data.opening_hours.hours.close}</h4>
+            <h4>{restaurant.opening_hours.hours.open}</h4>
+            <h4>{restaurant.opening_hours.hours.close}</h4>
           </div>
         )}
 
-      {data && data.pickup && <div>{data.pickup ? 'pickup' : null} </div>}
-      {data && data.delivery && <div>{data.delivery ? 'delivery' : null}</div>}
+      {restaurant && restaurant.pickup && (
+        <div>{restaurant.pickup ? 'pickup' : null} </div>
+      )}
+      {restaurant && restaurant.delivery && (
+        <div>{restaurant.delivery ? 'delivery' : null}</div>
+      )}
     </div>
-  );
+  ) : null;
 }
-
-// --------------------------------------------------------
-//   return (
-//     <div style={{ color: 'red' }}>
-//       <h1>{data?.name || 'there is no id'}</h1>
-//       <h2>{data?.cuisine || 'there is no id'}</h2>
-//       <h5>{data?.formatted_address || 'there is no id'}</h5>
-//       {/* <h5>
-//         {data?.social.email || 'there is no id'}
-//         {data?.social.phone || 'there is no id'}
-//       </h5> */}
-//       <img src={data?.photos[0].links[0] || 'there is no id'} alt='icon' />
-//       <h6>
-//         {data?.price_level || 'there is no id'} ||{' '}
-//         {data?.rating || 'there is no id'}
-//       </h6>
-//       <div>
-//         {data?.opening_hours.open_now ? 'open' : 'closed' || 'there is no id'}{' '}
-//       </div>
-//       <div>{data?.pickup ? 'pickup' : null || 'there is no id'} </div>
-//       <div>{data?.delivery ? 'delivery' : null || 'there is no id'}</div>
-//     </div>
-//   );
-// }
-
 export default RestaurantDetails;
-
-/* {data.results.map((rest) => (
-        <div>
-          <h3>{`Name: ${rest.name}`}</h3>
-          <h4>{`Cuisine: ${rest.cuisine} `}</h4>
-          <h5>{rest.formatted_address}</h5>
-          <h5>{`Email: ${rest.social.email}  || Phone: ${rest.social.phone}`}</h5>
-          <img src={rest.photos[0].links[0]} alt='icon' />
-          <h6>{`Price level ${rest.price_level} ||  Rating ${rest.rating}`}</h6>
-          <div>{rest.opening_hours.open_now ? 'open' : 'closed'} </div>
-          <div>{rest.pickup ? 'pickup' : null} </div>
-          <div>{rest.delivery ? 'delivery' : null}</div>
-          <h5>--------------------------------------------------</h5>
-        </div>
-      ))} */
