02668c20b8c05fb2fa4d5aad558adb1f4f9d2273
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-06-16 21:29:00 +0200

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,20 @@ import React from 'react';
 const About = () => {
   return (
     <div className='about-page'>
-      <h1>Localista :)</h1>
-      <p>the Final project of the React.js Redi-school course </p>
-      <h2>
-        This project is made by:
-        <br /> <br /> <br /> Amjad Oudeh & Sitora Sodatkadamova
-      </h2>
+      <h1 className='about-title'>Localista :)</h1>
+      <div className='restaurant-owners'>
+        <img src='' alt='' />
+        <div className='restaurant-owner-info'>
+          <p></p>
+        </div>
+      </div>
+      <div className='app-owner'>
+        <p>the Final project of the React.js Redi-school course </p>
+        <h2>
+          This project is made by:
+          <br /> <br /> <br /> Amjad Oudeh & Sitora Sodatkadamova
+        </h2>
+      </div>
     </div>
   );
 };
