4883024ec92475d38ef8166e25bf75db32467670
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-06-17 16:05:44 +0200

    the final touch

--- src/components/ListOfRestaurants.js
+++ src/components/ListOfRestaurants.js
@@ -74,67 +74,70 @@ function ListOfRestaurants() {
 
   return (
     <div className='big-list'>
-      <div>
+      <div className='main-img'>
         <img src={HomePageGif} className='HomepageGif' alt='HomePaGeGif' />
-        <div className='div-under-gif'>
-          <h3 className='text-under-gif'>Berlin local restaurants</h3>
-          <div className='Search-Buttons'>
-            <input
-              autoFocus='off'
-              autoComplete='off'
-              className='Search-bar'
-              type='text'
-              placeholder='Find your restaurant'
-              onChange={onSearchedRestaurants}
-            />
-            <div className='Buttons'>
-              <button className='FilterButton' onClick={handleFilterAll}>
-                All{' '}
-              </button>
-              <button className='FilterButton' onClick={handleFilterOpen}>
-                Open{' '}
-              </button>
-              <button className='FilterButton' onClick={handleFilterPickup}>
-                Pickup{' '}
-              </button>
-              <button className='FilterButton' onClick={handleFilterDelivery}>
-                Delivery{' '}
-              </button>
-              <button className='FilterButton' onClick={handleFilterClose}>
-                Closed
-              </button>
+        <div className='main'>
+          <div className='div-under-gif'>
+            <h3 className='text-under-gif'>Berlin local restaurants</h3>
+            <div className='Search-Buttons'>
+              <input
+                autoFocus='off'
+                autoComplete='off'
+                className='Search-bar'
+                type='text'
+                placeholder='Find your restaurant'
+                onChange={onSearchedRestaurants}
+              />
+              <div className='Buttons'>
+                <button className='FilterButton' onClick={handleFilterAll}>
+                  All{' '}
+                </button>
+                <button className='FilterButton' onClick={handleFilterOpen}>
+                  Open{' '}
+                </button>
+                <button className='FilterButton' onClick={handleFilterPickup}>
+                  Pickup{' '}
+                </button>
+                <button className='FilterButton' onClick={handleFilterDelivery}>
+                  Delivery{' '}
+                </button>
+                <button className='FilterButton' onClick={handleFilterClose}>
+                  Closed
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className='restaurants'>
-        {filteredRestaurants &&
-          filteredRestaurants.map((restaurant) => (
-            <div className='restaurant' key={restaurant.id}>
-              <div className='restaurant-image'>
-                <img
-                  // style={{ width: 300, height: 300 }}
-                  src={restaurant.photos[0].links[0]}
-                  alt=''
-                />
-              </div>
-              <div className='restaurant-info'>
-                <h1 className='restaurant-title'>
-                  <Link to={`/ListOfRestaurants/${restaurant.id}`}>
-                    {restaurant.name}
-                  </Link>
-                </h1>
 
-                <h3 className='restaurant-cuisine'>{restaurant.cuisine}</h3>
-                <h4 className='restaurant-rating'>
-                  Rating:{restaurant.rating}
-                </h4>
-                <h4 className='restaurant-price-level'>
-                  Price level: {restaurant.price_level}
-                </h4>
-              </div>
-            </div>
-          ))}
+          <div className='restaurants'>
+            {filteredRestaurants &&
+              filteredRestaurants.map((restaurant) => (
+                <div className='restaurant' key={restaurant.id}>
+                  <div className='restaurant-image'>
+                    <img
+                      // style={{ width: 300, height: 300 }}
+                      src={restaurant.photos[0].links[0]}
+                      alt=''
+                    />
+                  </div>
+                  <div className='restaurant-info'>
+                    <h1 className='restaurant-title'>
+                      <Link to={`/ListOfRestaurants/${restaurant.id}`}>
+                        {restaurant.name}
+                      </Link>
+                    </h1>
+
+                    <h3 className='restaurant-cuisine'>{restaurant.cuisine}</h3>
+                    <h4 className='restaurant-rating'>
+                      Rating:{restaurant.rating}
+                    </h4>
+                    <h4 className='restaurant-price-level'>
+                      Price level: {restaurant.price_level}
+                    </h4>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
